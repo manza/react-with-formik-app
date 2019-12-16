c4b917a5bb8be515318139bb03ba3479c3adafac
Reinaldo Manzano Junior <reinaldo.manzano@gmail.com>
2019-12-16 14:50:52 -0300

    reformatting code for PPT

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,11 +4,7 @@ import './LoginForm.css';
 class LoginForm extends Component {
 
     // 1 setup state in order to deal with form data
-    state = {
-        errorUser: '',
-        userEmail: '',
-        password: ''
-    };
+    state = { errorUser: '', userEmail: '', password: '' };
 
     // 2 setup function to deal with Form submission
     handleSubmit = event => {
@@ -18,9 +14,7 @@ class LoginForm extends Component {
     }
 
     // 3.1 setup function to deal with input field userEmail
-    handleUserEmailChange = event => {
-        this.setState( {userEmail: event.target.value} );
-    }
+    handleUserEmailChange = event => this.setState( {userEmail: event.target.value} );
 
     // 3.2 setup function to deal with input field userEmail validations & handle span for showing errors
     handleUserEmailBlur = event => {
@@ -32,35 +26,23 @@ class LoginForm extends Component {
         }
     }
 
-    // setup function to deal with input field password
-    handlePasswordChange = event => {
-        this.setState( {password: event.target.value} );
-    }
+    // 4 setup function to deal with input field password
+    handlePasswordChange = event => this.setState( {password: event.target.value} );
 
     render() {
       const { errorUser, userEmail, password } = this.state;
-
       return (
         <form className="Form" onSubmit={this.handleSubmit}>
             <h2 className="Form-Welcome">Formik & Yup</h2>
             <h2 className="Form-Info">Form managment and validations</h2>
             <div className="Form-Group">
-                <input className="Form-Field"
-                       name="userEmail"
-                       placeholder="User"
-                       type="input"
-                       value={userEmail}
-                       onChange={this.handleUserEmailChange}
-                       onBlur={this.handleUserEmailBlur}
+                <input className="Form-Field" name="userEmail" placeholder="User" type="input" value={userEmail}
+                       onChange={this.handleUserEmailChange} onBlur={this.handleUserEmailBlur}
                 />
                 {errorUser !== '' ? <span className="Form-Error">{errorUser}</span> : ''}
             </div>
             <div className="Form-Group">
-                <input className="Form-Field"
-                       name="password"
-                       placeholder="Password"
-                       type="password"
-                       value={password}
+                <input className="Form-Field" name="password" placeholder="Password"  type="password" value={password} 
                        onChange={this.handlePasswordChange} />
             </div>
             <button className="Form-Btn" type="submit">Login</button>
